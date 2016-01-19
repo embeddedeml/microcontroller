845202416a02d2aa4c68d761fadedc050823a227
embeddedeml <does@not.matter>
2016-01-19 20:29:02 +0100

    support for additional timer channels

--- base/Timer/Timer.h
+++ base/Timer/Timer.h
@@ -10,6 +10,16 @@
 #include "BaseDef.h"
 
 extern void Timer_Timer1_Interrupt(void);
-extern void Timer_Timer1_Init(void);
+extern void Timer_Timer2_Interrupt(void);
+extern void Timer_Timer3_Interrupt(void);
+
+extern void Timer_Timer1_Start(uint16_t intervall_us);
+extern void Timer_Timer1_Stop(void);
+
+extern void Timer_Timer2_Start(uint16_t intervall_us);
+extern void Timer_Timer2_Stop(void);
+
+extern void Timer_Timer3_Start(uint16_t intervall_us);
+extern void Timer_Timer3_Stop(void);
 
 #endif /* TIMER_TIMER_H_ */
